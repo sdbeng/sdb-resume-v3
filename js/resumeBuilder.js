6b126b06b69cfddd9238ad4cc652470bbb592432
Sergio Barrera <sdbeng@gmail.com>
2018-07-30 14:33:53 -0700

    Add languages to work exp

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,11 +3,11 @@ var work = {
   jobs: [
     {
       employer: "Trilogy-UC BErkeley",
-      title: "Junior Full Stack Web Developer",
+      title: "Full Stack Web Developer",
       location: "Berkeley, CA",
       dates: "2017 - Present",
       description:
-        "Providing Bootcamp support to a group of talented undergraduate individuals who pursue a career change in their lives. Lead discussions, provide feedback and guide students in their daily coding activities and challenges. Mainly building web applications with HTML, CSS, JavaScript and React plus SQL/MongoDB. Being an active mentor in their 7-month journey to succeed in 4 final projects."
+        "Providing Bootcamp support to a group of talented undergraduate individuals who pursue a career change in their lives. Lead discussions, review code, track down Git issues and provide feedback on their projects. Guide students in their daily coding activities and challenges. Building web applications with HTML, CSS, JavaScript and React plus databases like SQL & Mongodb. Django and Node as back-end. Being an active mentor in their 7-month journey to succeed in 4 final projects."
     },
     {
       employer: "SDB Webs",
@@ -15,7 +15,7 @@ var work = {
       location: "Concord, CA",
       dates: "2009 - present",
       description:
-        "Building web applications to solve different problems when trying to access data on the web or from iOS and Android devices. As contracted Front End Developer for a National Two-Way Bilingual Org. I developed 4 iOS iPhone Apps that were pushed to the App Store for the Annual National TWBI Conference. (http://goo.gl/TmwMyH). Re-designed and crafted their front end website and kept up to date according to their CEO guidelines. Created Call for Papers angularjs front and back end to collect Conference presenters data. Daily work on HTML, CSS and JavaScript.Also, CMS development for some internal projects. "
+        "Currently, working on a new React project development. Previous experience building web applications to solve different problems when trying to access data on the web or from iOS and Android devices. As contracted Front End Developer for a National Two-Way Bilingual Org. I developed 4 iOS iPhone Apps that were pushed to the App Store for the Annual National TWBI Conference. (http://goo.gl/TmwMyH). Re-designed and crafted their front end website and kept up to date according to their CEO guidelines. Created Call for Papers angularjs front and back end to collect Conference presenters data. Daily work on HTML, CSS and JavaScript.Also, CMS development for some internal projects. "
     }
   ]
 };
